441dc637676e2d5b9206a5cf7223f19f4f6227f4
Lily Barrett <lily.barrett@ef.com>
2017-12-19 21:49:45 -0500

    Add sun, WIP lighting

--- index.vr.js
+++ index.vr.js
@@ -4,11 +4,13 @@ import {
   asset,
   Pano,
   Text,
+  DirectionalLight,
   Sphere,
   View,
 } from 'react-vr';
 import { Provider } from "react-redux";
 import Earth from "./components/earth/earth";
+import Sun from "./components/sun/sun";
 import Moon from "./components/moon/moon";
 import Nav from "./components/navigation/nav";
 import store from "./redux/store";
@@ -20,16 +22,23 @@ export default class world_explorer extends React.Component {
         <View>
           <View>
             <Pano source={asset('star_bg.jpg')}/>
-            <Earth scale={1.5} />
+            <Earth scale={1.5} lit={true}/>
             <Nav />
           </View>
-          <View style={{ 
+          <View style={{
               position:"absolute",
               transform: [
                 { translate: [-5, -5, -5]  }
               ]
           }}>
             <Moon />
+          </View>
+          <DirectionalLight intensity={20} style={{ transform: [{ rotateZ: -35 }] }}/>
+          <View style={{
+            position:"absolute",
+            transform: [{translate: [-3, -7, -1]}],
+          }}>
+            <Sun />
           </View>
         </View>
       </Provider>
