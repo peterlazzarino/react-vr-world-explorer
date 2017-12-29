dd718bc8bccdb8a0c3975e21f7e6fa131d4c1b6e
peter.lazzarino <peter.lazzarino@ef.com>
2017-12-29 11:51:22 -0500

    add native line module and react component

--- vr/client.js
+++ vr/client.js
@@ -1,18 +1,13 @@
-// Auto-generated content.
-// This file contains the boilerplate to set up your React app.
-// If you want to modify your application, start in "index.vr.js"
+import RCTLine from "../native-views/RCTLine";
 
-// Auto-generated content.
-import {VRInstance} from 'react-vr-web';
+import { VRInstance } from 'react-vr-web';
 
 function init(bundle, parent, options) {
   const vr = new VRInstance(bundle, 'world_explorer', parent, {
-    // Add custom options here
+    customViews: [{ name: "VRLine", view: RCTLine}],
     ...options,
   });
-  vr.render = function() {
-    // Any custom behavior you want to perform on each frame goes here
-  };
+  vr.render = function() { };
   vr.start();
   return vr;  
 }
